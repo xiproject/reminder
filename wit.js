d4d0f49e07bd1e92931211a8ed51e191172cfa20
Mihir Rege <mihirrege@gmail.com>
2014-12-02 15:15:09 +0530

    Cleanup

--- wit.js
+++ wit.js
@@ -1,50 +1,56 @@
-var request = require('request').defaults({
-    proxy: 'http://10.3.100.207:8080'
-});
+var request;
+var config;
+var token;
+var fs = require('fs');
 
-var token = require('./config.json').wit;
-
-var message = 'Remind me to slap vikrant in 4 minutes';
-
-
-
+if (fs.existSync('./config.json')) {
+    config = require('./config.json');
+    token = config.wit;
+    if (config.proxy) {
+        request = require('request').defaults({
+            proxy: config.proxy
+        });
+    } else {
+        request = require('request');
+    }
+}
 
 function parseEntity(obj) {
-    var output = {}
-    
-    output.start = obj['start'];
-    output.end = obj['end'];
-    output.value = obj['value'];
-    output.body = obj['body'];
-    return output
-};
+    var output = {};
+    output.start = obj.start;
+    output.end = obj.end;
+    output.value = obj.value;
+    output.body = obj.body;
+    return output;
+}
 
 function parseResult(obj) {
-    var outcome = obj['outcomes'][0];
+    var outcome = obj.outcomes[0];
     var output = {};
     output.raw = obj;
-    output.text = obj['_text'];
-    output.intent = outcome['intent'];
-    output.confidence = outcome['confidence'];
+    output.text = obj._text;
+    output.intent = outcome.intent;
+    output.confidence = outcome.confidence;
     output.entities = {};
-    for (var name in outcome['entities']) {
-        output.entities[name] = parseEntity(outcome['entities'][name][0]);
+    for (var name in outcome.entities) {
+        output.entities[name] = parseEntity(outcome.entities[name][0]);
     }
     return output;
-};
+}
 
-//Does not support parsing time intervals
-function parse(input, cb){
+//TODO Does not support parsing time intervals
+function parse(input, cb) {
+    if(!token){
+        return;
+    }
     var options = {
-        uri: 'https://api.wit.ai/message?q='+ encodeURIComponent(input),
+        uri: 'https://api.wit.ai/message?q=' + encodeURIComponent(input),
         headers: {
             'Authorization': 'Bearer ' + token
         },
     };
 
     request(options, function(error, response, body) {
-
-        console.log(body);
         cb(parseResult(JSON.parse(body)));
     });
 }
